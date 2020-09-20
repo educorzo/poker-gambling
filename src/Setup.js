a3f1ae0437895c3fa583a97b70920b601b570481
Eduardo Corzo <edukorzo@gmail.com>
2020-09-20 18:36:52 +0200

    Reveal two random hands

--- src/Setup.js
+++ src/Setup.js
@@ -2,9 +2,14 @@ import Hand from './Hand.js';
 
 export default class Setup {
   init(resources, app) {
-    let hand = new Hand(resources.cards.textures);
+    let hand = new Hand(resources.cards.textures),
+     hand2 = new Hand(resources.cards.textures);
     hand.fillCards();
     hand.reveal();
+    hand2.y = 300;
+    hand2.fillCards();
+    hand2.reveal();
     app.stage.addChild(hand);
+    app.stage.addChild(hand2);
   }
 }
