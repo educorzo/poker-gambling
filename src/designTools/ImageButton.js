3e6d9b83d66208b3125113010acfacb002946ac3
Eduardo Corzo <edukorzo@gmail.com>
2020-10-17 15:11:00 +0200

    Adding touchness to buttons

--- src/designTools/ImageButton.js
+++ src/designTools/ImageButton.js
@@ -9,5 +9,15 @@ export default class ImageButton extends PIXI.Sprite {
     this.id = id;
 
     this.addChild(this.button);
+    this.on('mousedown', this._onMouseDown.bind(this));
+    this.on('mouseup', this._onMouseUp.bind(this));
+  }
+
+  _onMouseDown() {
+    this.button.tint = 0xDDDDDD;
+  }
+
+  _onMouseUp() {
+    this.button.tint = 0xFFFFFF;
   }
 }
