d868a97ac59e8c6d1a5626640f74abd92a65a77d
Eduardo Corzo <edukorzo@gmail.com>
2020-10-31 16:43:41 +0100

    Update logo

--- src/designTools/LogoBanner.js
+++ src/designTools/LogoBanner.js
@@ -2,22 +2,23 @@ var PIXI = require('pixi.js');
 import Text from './Text.js';
 
 export default class LogoBanner extends PIXI.Container {
-    constructor(text, width, height) {
+    constructor(text) {
         super();
 
-        this.text = new Text(text);
-        this.background = new PIXI.Graphics();
-        
-        this.addChild(this.background);
-        this.addChild(this.text);
+        this._text = new Text(text);
+        this._background = new PIXI.Graphics();
+
+        this.addChild(this._background);
+        this.addChild(this._text);
     }
 
     fitSize(width, height) {
-        this.background.beginFill(0x000000);
-        this.background.drawRect(0, 0, width, height);
-        this.background.endFill();
-        this.text.x = width / 2;
-        this.text.y = height / 2;
-        this.text.pivot.set(this.text.width / 2, this.text.height / 2);
+        this._background.beginFill(0x000000);
+        this._background.drawRect(0, 0, width, height);
+        this._background.endFill();
+        this._text.scale.set(1.5, 1.5);
+        this._text.x = width / 2;
+        this._text.y = height / 2;
+        this._text.pivot.set(this._text.width / 2 / this._text.scale.x, this._text.height / 2 / this._text.scale.y);
     }
 }
