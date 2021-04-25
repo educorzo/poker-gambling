ce50dce584ec1343a5937585beb39c6fce760fc0
Eduardo Corzo <edukorzo@gmail.com>
2021-04-25 12:11:07 +0200

    Small refactor

--- src/designTools/Button.js
+++ src/designTools/Button.js
@@ -3,16 +3,23 @@ import ButtonTextStyle from './ButtonTextStyle.js';
 
 export default class Button extends PIXI.Graphics {
   constructor(title, id) {
-    let cornerRadius = 5,
-      padding = 0,
-      defaultButton = {};
+    let defaultButton = {};
 
     super();
     this.textStyle = new ButtonTextStyle();
     defaultButton = new PIXI.Text('DEFAULT', this.textStyle);
 
     this._addText(title);
-    padding = this._buttonText.height / 2;
+    this._setStyle(defaultButton);
+    
+    this.interactive = true;
+    this.id = id;
+    this.on('pointerdown', this._onMouseDown.bind(this));
+    this.on('pointerup', this._onMouseUp.bind(this));
+  }
+  
+  _setStyle(defaultButton){
+    let cornerRadius = 5;
 
     this.beginFill(0xe8d6d5, 1);
     this._bottonButton = this.drawRoundedRect(this.x, this.y, defaultButton.width, defaultButton.height + defaultButton.height / 2, cornerRadius);
@@ -25,11 +32,6 @@ export default class Button extends PIXI.Graphics {
     this._buttonText.anchor.set(0.5, 0.5);
     this._buttonText.x = defaultButton.width / 2 ;
     this._buttonText.y = (defaultButton.height + defaultButton.height / 2.5)/2;
-
-    this.interactive = true;
-    this.id = id;
-    this.on('pointerdown', this._onMouseDown.bind(this));
-    this.on('pointerup', this._onMouseUp.bind(this));
   }
 
   _addText(title) {
